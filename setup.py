687c9fc188206be35055ccbc6a2ed17357b1d84e
Siracenco Serghei <108734530+SiracencoSerghei@users.noreply.github.com>
2023-10-19 20:09:09 +0200

    upgrade

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='clean_folder_sergio_09_78',
-    version='1.0',
+    version='1.0.1',
     description='Clean folder script',
     author='Siracenco Serghei',
     author_email='siracencoserghei@gmail.com',
